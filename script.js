54be66c6f1fdbfe9f183b8a9b77daa5bf0cae22f
GigiMelhorQueRenatao <gisele.sts@hotmail.com>
2025-06-11 18:01:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// 💄 Script mais afiado que a unha da Gigi 💅
+const frases = [
+  "Renatão nunca mais vai humilhar a gigi",
+  "Ele só tá com inveja pq a Gigi sabe fazer LP e ele não",
+  "A Gigi fala quando quiser com o renatão",
+  "Vai chorar?? Pois chore! #GigiBela"
+];
+
+const ul = document.getElementById("frases");
+
+frases.forEach((frase) => {
+  const li = document.createElement("li");
+  li.textContent = frase;
+  ul.appendChild(li);
+});
+
+// ✨ Feito por Gigi Pirigótica, a melhor de todas e todos!
